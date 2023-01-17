835f28cf17faa651ea1880318fd6be85fd84d93c
Isabela Costa <isa-hcosta@hotmail.com>
2023-01-17 10:41:03 -0300

    arruma requisito 6

--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -1,21 +1,9 @@
-const { salesModel, productsModel } = require('../models');
+const { salesModel } = require('../models');
 const validationsInput = require('./validations/validationsInput');
 
 const create = async (body) => {
   const { type, message } = validationsInput.validateCreateSale(body);
   if (type) return { type, message };
-
-  const isAllInDb = await Promise.all(
-    body.map(async (e) => {
-      const product = await productsModel.findById(e.productId);
-      if (!product) return 'no';
-      return 'yes';
-    }),
-  );
-
-  if (isAllInDb.some((e) => e === 'no')) {
-    return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
-  }
 
   const result = await salesModel.create(body);
 
